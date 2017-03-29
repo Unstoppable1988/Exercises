9acc4530561b533962c9b6b6f616c62a5339189b
Unstoppable1988 <Termo1988@mail.ru>
2017-03-29 22:50:58 +0300

    New: Exercise Factorial #1

---
+++ Factorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Factorial
+    {
+       public static void Run()
+        {
+            int chislo;
+
+            int factorial = 1;
+
+            Console.WriteLine("Введите число");
+
+            chislo = int.Parse(Console.ReadLine());
+
+            for (int x = chislo; x > 1; x--)
+
+                factorial = factorial * x;
+
+            Console.WriteLine("Факториал  = " + factorial);
+
+
+            Console.ReadLine();
+        }
+    }
+}
