d9d028027e11ef1ad1f774cdae1a9c7c096676fd
Unstoppable1988 <Termo1988@mail.ru>
2017-03-30 16:26:48 +0300

    New: Squares 2 of 3 exercise #3

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Exercises
     
         static void Main(string[] args)
         {
-            Factorial.Run();
+            Squares_2_of_3.Run();
 
             
         }
---
+++ Squares 2 of 3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Squares_2_of_3
+    {
+        public static void Run()
+        {
+            int a;
+
+            int b;
+
+            int c;
+
+            int square;
+
+            Console.WriteLine("Введите первое число");
+
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число");
+
+            c = int.Parse(Console.ReadLine());
+
+            if ((a >= b) && (b >= c))
+            {
+                square = a * a + b * b;
+            }
+            else if ((b >= c) && (c >= a))
+            {
+                square = b * b + c * c;
+            }
+            else if ((c >= a) && (a >= b))
+            {
+                square = c * c + a * a;
+            }
+            else if ((a <= b) && (b <= c))
+            {
+                square = b * b + c * c;
+            }
+            else
+
+                square = 0;
+                Console.WriteLine("Squares sum  = " + square);
+
+
+            Console.ReadLine();
+        }
+    }
+}
