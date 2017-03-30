0aaefe3eec5d272093ba4ad895e57e71e82262dc
Unstoppable1988 <Termo1988@mail.ru>
2017-03-30 21:18:16 +0300

    New: Prime factor exercise #5

---
+++ Prime Factor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Prime_Factor
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Введите число");
+
+            decimal chislo;
+
+            chislo = decimal.Parse(Console.ReadLine());
+
+            for (decimal z = 2; z < chislo; z++)
+            {
+                if (chislo%z == 0)
+                {
+                    chislo = chislo / z;
+                } 
+            }
+            Console.WriteLine(chislo);
+
+            Console.ReadLine();
+        }
+    }
+}
