b9c165c8846aa25640fa34844aaa55a8ff3c4bae
Unstoppable1988 <Termo1988@mail.ru>
2017-03-30 22:28:21 +0300

    New: Version of Factor #5

---
+++ Prime Factor ver2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Prime_Factor_ver2
+    {
+        static bool IsFactor(int z, int chislo)
+        {
+          return chislo % z == 0; 
+        };
+
+        static bool IsPrime(int num)
+
+        {
+
+        }
+
+        public static void Run()
+        {
+            
+
+            Console.WriteLine("Введите число");
+
+            decimal chislo;
+
+            chislo = decimal.Parse(Console.ReadLine());
+
+            for (decimal z = 2; z < chislo; z++)
+
+            { 
+                if (chislo%z == 0)
+                {
+                    chislo = chislo / z;
+                } 
+            }
+            Console.WriteLine(chislo);
+
+            Console.ReadLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Exercises
     
         static void Main(string[] args)
         {
-            Prime_Factor.Run();
+            Prime_Factor_ver2.Run();
 
             
         }
