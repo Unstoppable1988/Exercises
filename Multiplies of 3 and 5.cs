0e7ec5bdd66e96680fa1ac2eb4efeb197929e86b
Unstoppable1988 <Termo1988@mail.ru>
2017-03-30 12:28:33 +0300

    New: Multiplies 3 and 5 exercise #2

---
+++ Multiplies of 3 and 5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Multiplies_of_3_and_5
+    {
+       public static void Run()
+        {
+            int x = 0;
+
+            int a = 0;
+
+            int i;
+
+            for (int y = x; y < 1000; y=y+3)
+                
+                x = x + y;
+
+            for (int b = a; b < 1000; b = b + 5)
+
+                a = a + b;
+
+            i = a + x;  
+
+
+                Console.WriteLine("Multipies sum  = " + i);
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Exercises
     
         static void Main(string[] args)
         {
-            Factorial.Run();
+            Multiplies_of_3_and_5.Run();
 
             
         }
