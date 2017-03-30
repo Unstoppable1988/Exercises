e7f06dc5f124292448d684a1f02ec47dfcf46ce7
Unstoppable1988 <Termo1988@mail.ru>
2017-03-30 17:05:25 +0300

    New: Fibonachi exercise #4

---
+++ Fibonachi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Fibonachi
+    {
+       public static void Run()
+        {
+            int pervoe = 1;
+
+            int vtoroe = 1;
+
+            int predel;
+
+           Console.WriteLine("Введите количество чисел фибоначи");
+
+            predel = int.Parse(Console.ReadLine());
+
+            for (int x = 0; x > predel; x++)
+            {
+               int ffibonachi = pervoe + vtoroe;
+
+                pervoe = vtoroe;
+
+                vtoroe = ffibonachi;
+             
+            }
+              
+            Console.WriteLine("Fibonachi  = " + ffibonachi);
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Exercises
     
         static void Main(string[] args)
         {
-            Squares_2_of_3.Run();
+            Fibonachi.Run();
 
             
         }
