9cb3a8bd95d8c3544a5020032b6159003360b795
Unstoppable1988 <Termo1988@mail.ru>
2017-04-10 17:00:33 +0300

    New: Palindrome #7

---
+++ Palindrome.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Palindrome
+    {
+        static int Chislo()
+
+        {
+
+            for (int x = 100; x < 1000; x++)
+            {
+                for (int y = 100; y < 1000; y++)
+                {
+                    return x * y;
+                }
+                string s = x.ToString();
+                if (s[5] == s[0]) { .. }
+            }
+        }
+
+
+
+
+
+        Console.WriteLine();
+            Console.ReadLine();
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Exercises
     
         static void Main(string[] args)
         {
-            Recursion.Run();
+            Palindrome.Run();
 
             
         }
