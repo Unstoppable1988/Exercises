ab30c10b05e98c84ada1f68165455587202edd33
Unstoppable1988 <Termo1988@mail.ru>
2017-04-12 22:33:18 +0300

    New: Sum square difference exercise #9

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Exercises
     
         static void Main(string[] args)
         {
-            Smalest_multiplie.Run();
+            Sum_square_difference.Run();
 
             
         }
---
+++ Sum square difference.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Sum_square_difference
+    {
+        public static int SumSquare(int z)
+        {
+            int b = 0;
+
+            for (int a = 1; a <= z; a++)
+            {
+                b = (a * a) + b;
+            }
+            return b;
+        }
+        public static int SquareSum(int y)
+        {
+            int b = 0;
+
+            for (int a = 1; a <= y; a++)
+            {
+                b = a + b;
+            }
+            return b * b;
+        }
+        public static void Run()
+        {
+        Console.WriteLine(SquareSum(100)-SumSquare(100));
+        Console.ReadLine();
+        }
+    }
+}
+
+
+
+
