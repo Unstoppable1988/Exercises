6ff7cc3f42f30e027b04efef6fcc06d0107ebb22
Unstoppable1988 <Termo1988@mail.ru>
2017-04-18 18:00:15 +0300

    New: Replace word exercise #10

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Exercises
     
         static void Main(string[] args)
         {
-            Sum_of_primes_2.Run();
+            Replace_word.Run();
 
             
         }
--- Replace word.cs
+++ Replace word.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 
 
-namespace Exercise
+namespace Exercises
 {
-    class Replace
+    class Replace_word
     {
-        string[] list = new string[] { "1 2 13 3 1 4 1" };
-        
+        public static int[] Replace(int[] number)
+        {
+            for (int a = 0; a < number.Length; a++)
+            {
+                if (number[a] == 1)
+                {
+                    number[a] = 2;
+                }
+            }
+            return number;
+        }
+        public static void Run()
+        {
+            int[] nums = Replace(new int[] { 1, 2, 3, 1, 4, 5, 1 });
+            for (int x = 0; x < nums.Length; x++)
+            {
+                Console.WriteLine(nums[x]);
+                
+            }
+            Console.ReadLine();
+        }
     }
 }
