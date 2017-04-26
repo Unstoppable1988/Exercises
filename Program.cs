a5ec6844ddfd7b8d0e1d96e55fae881e485b7177
Unstoppable1988 <Termo1988@mail.ru>
2017-04-26 14:05:09 +0300

    Add: Sort array reverse exercise #15

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Exercises
     
         static void Main(string[] args)
         {
-            Sort_array.Run();
+            Sort_array_reverse.Run();
 
             
         }
---
+++ Sort array reverse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Sort_array_reverse
+    {
+        public static int[] Array(int[] numbers)
+        {
+            for (int x = 0; x < numbers.Length; x++)
+            {
+                for (int z = 0; z < numbers.Length - 1; z++)
+                {
+                    if (numbers[z] < numbers[z + 1])
+                    {
+                        int bubble = numbers[z + 1];
+                        numbers[z + 1] = numbers[z];
+                        numbers[z] = bubble;
+                    }
+                }
+            }
+            return numbers;
+        }
+        public static void Run()
+        {
+            int[] nums = Array(new int[] { 3, 5, 2, 6, 1 });
+            for (int x = 0; x < nums.Length; x++)
+            {
+                Console.WriteLine(nums[x]);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
