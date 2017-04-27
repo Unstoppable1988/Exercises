e3478f934b85b547fb29a2b62389ec0026dc1412
Unstoppable1988 <Termo1988@mail.ru>
2017-04-27 21:57:49 +0300

    New: Array practice 1 exercise #17

---
+++ Array practice 1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Array_practice_1
+    {
+        public static int[] Array()
+        {
+            Console.WriteLine("Enter the size of array");
+            int size = int.Parse(Console.ReadLine());
+            int[] list = new int[size];
+            Random randomArrayNumbers = new Random();
+            for (int z = 0; z < list.Length; z++)
+            {
+                list[z] = randomArrayNumbers.Next(0,1000);
+            }
+            return list;
+        }
+        public static void Run()
+        {
+            int[] nums = Array();
+            for (int x = 0; x < nums.Length; x++)
+            {
+                Console.WriteLine(nums[x]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Exercises
     
         static void Main(string[] args)
         {
-            Triangle_Number.Run();
+            Array_practice_1.Run();
 
             
         }
