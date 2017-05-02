0aa25f9790873f7daac2f3171ffc6f4887f62d1e
Unstoppable1988 <Termo1988@mail.ru>
2017-05-02 21:20:48 +0300

    Fix: Array practice 8 exercise #24

--- Array practice 8.cs
+++ Array practice 8.cs
@@ -39,33 +39,34 @@ namespace Exercises
             {
                 newArray[z] = oldArray[z];
             }
-            newArray[0] = new int[1] { positionNumber + 1 };
-            newArray[1] = new int[1] { number };
+            newArray[newArray.Length - 1] = new int[2];
+            newArray[newArray.Length - 1][0] = positionNumber;
+            newArray[newArray.Length - 1][1] = number;
             return newArray;
         }
-        public static void Run()
+    public static void Run()
+    {
+        int[] nums = CreateRandomArray();
+        Console.WriteLine("Your random created array is:");
+        for (int x = 0; x < nums.Length; x++)
         {
-            int[] nums = CreateRandomArray();
-            Console.WriteLine("Your random created array is:");
-            for (int x = 0; x < nums.Length; x++)
-            {
-                Console.WriteLine(nums[x]);
-            }
-            Console.WriteLine("Enter number to find it index into array");
-            int number = int.Parse(Console.ReadLine());
-            Console.WriteLine("Position of the number into array is:");
-            int[][] newNums = FindNumberIntoArray(nums, number);
-            for (int i = 0; i < newNums.Length; i++)
-            {
-                Console.Write("Element({0}): ", i);
-
-                for (int j = 0; j < newNums[i].Length; j++)
-                {
-                    Console.Write("{0}{1}", newNums[i][j], j == (newNums[i].Length - 1) ? "" : " ");
-                }
-                Console.WriteLine();
-            }                        
-            Console.ReadLine();
+            Console.WriteLine(nums[x]);
         }
+        Console.WriteLine("Enter number to find it index into array");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Position of the number into array is:");
+        int[][] newNums = FindNumberIntoArray(nums, number);
+        for (int i = 0; i < newNums.Length; i++)
+        {
+            Console.Write("Element({0}): ", i);
+
+            for (int j = 0; j < newNums[i].Length; j++)
+            {
+                Console.Write("{0}{1}", newNums[i][j], j == (newNums[i].Length - 1) ? "" : " ");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
     }
+}
 }
