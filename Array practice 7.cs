b8ef10b8a0dc1393d8b3f22b75ef80a4fa9fffd6
Unstoppable1988 <Termo1988@mail.ru>
2017-05-02 16:38:06 +0300

    New: Array practice 7 exercise #23

---
+++ Array practice 7.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Array_practice_7
+    {
+        public static int[] CreateRandomArray()
+        {
+            Console.WriteLine("Enter the size of array");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            Random randomArrayNumbers = new Random();
+            for (int y = 0; y < array.Length; y++)
+            {
+                array[y] = randomArrayNumbers.Next(0, 10);
+            }
+            return array;
+        }
+        public static int[] FindNumberIntoArray(int[] array, int number)
+        {
+            int position = 0;
+            for (int z = 0; z < array.Length; z++)
+            {
+                if (array[z] == number)
+                {
+                    position = position + 1;
+                    InsertIntoArray(position, z);
+                }
+            }
+            return array;
+        }
+        public static int[] InsertIntoArray(int position, int positionNumber)
+        {
+            int[] newArray = new int[position];
+            for (int z = 0; z < newArray.Length; z++)
+            {
+                newArray[z] = positionNumber;
+            }
+            return newArray;
+        }
+        public static void Run()
+        {
+            int[] nums = CreateRandomArray();
+            Console.WriteLine("Your random created array is:");
+            for (int x = 0; x < nums.Length; x++)
+            {
+                Console.WriteLine(nums[x]);
+            }
+            Console.WriteLine("Enter number to find it index into array");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Position of the number into array is:");
+            int[] newNums = FindNumberIntoArray(nums, number);
+            for (int x = 0; x < newNums.Length; x++)
+            {
+                Console.WriteLine(newNums[x]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
